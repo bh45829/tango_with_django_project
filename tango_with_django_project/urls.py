01afe9c6bfddbb7838b16dd8f8528f65e43ac374
Rory Hunter <2655888H@student.gla.ac.uk>
2023-01-19 18:20:31 +0000

    4.3 complete. media directory added and media context_processor added.

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import  settings
+from django.conf.urls.static import static
 from rango import views
 
 
@@ -22,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
