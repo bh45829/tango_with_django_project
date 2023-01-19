b76182cb28b936c668686e0a8415404080a06ee8
Rory Hunter <2655888H@student.gla.ac.uk>
2023-01-19 17:44:13 +0000

    4.1 complete. Render function utilised with index.html for index site

--- rango/views.py
+++ rango/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    html = "<html><body>Rango says hey there partner!<br><a href='/rango/about/'>About</a></body></html>"
-    return HttpResponse(html)
-
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 def about(request):
     html = "<html><body>Rango says here is the about page.<br><a href='/rango/'>Index</a></body></html>"
     return HttpResponse(html)
