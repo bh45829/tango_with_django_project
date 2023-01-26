734fcc452e6d82c7843601084600819edbebfa1f
Rory Hunter <04_cue.fewest@icloud.com>
2023-01-26 15:05:13 +0000

    Chapter 4 complete. About.html added with rango and cat

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
+
+
 def about(request):
-    html = "<html><body>Rango says here is the about page.<br><a href='/rango/'>Index</a></body></html>"
-    return HttpResponse(html)
+    context_dict = {'boldmessage': 'This tutorial has been put together by Rory Hunter'}
+    return render(request, 'rango/about.html', context=context_dict)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import  settings
+from django.conf import settings
 from django.conf.urls.static import static
 from rango import views
 
@@ -24,5 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
