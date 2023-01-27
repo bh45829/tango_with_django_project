069a24dbbb2cf272f4aedc9d73652bf706e85d4f
Rory Hunter <04_cue.fewest@icloud.com>
2023-01-27 20:52:44 +0000

    improved admin functionality (end of ch 5) (not finished)

--- rango/admin.py
+++ rango/admin.py
@@ -3,5 +3,19 @@ from rango.models import Category, Page
 
 # Register your models here.
 
-admin.site.register(Category)
+# admin.site.register(Category)
 admin.site.register(Page)
+
+
+class PageInline(admin.TabularInline):
+    model = Page
+    extra = 3
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name', 'views', 'likes']
+    inlines = [PageInline]
+    list_display = ('name', 'views', 'likes')
+    list_filter = ['views']
+    search_fields = ['name']
+
+admin.site.register(Category, CategoryAdmin)
