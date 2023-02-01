cdc10d649e72207b50d2e4696d228330babc33a8
Rory Hunter <04_cue.fewest@icloud.com>
2023-02-01 15:03:43 +0000

    finished chapter 6. passing tests

--- rango/views.py
+++ rango/views.py
@@ -6,16 +6,19 @@ from rango.models import Category, Page
 def index(request):
 
     category_list = Category.objects.order_by('-likes')[:5]
+    pages_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = pages_list
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Rory Hunter'}
     return render(request, 'rango/about.html', context=context_dict)
+
 
 def show_category(request, category_name_slug):
 
